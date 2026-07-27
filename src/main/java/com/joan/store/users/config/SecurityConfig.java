59064eaf017e8e116b38f3b550b520e12df0254b
jianna4 <mainajoan555@gmail.com>
2026-07-27 08:40:57 +0300

    security connfiguration,and removed jwt since i wasnt understanding it

---
+++ src/main/java/com/joan/store/users/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.joan.store.users.config;
+
+import com.joan.store.users.service.CustomUserDetailsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration //tells spring this class has beans
+@EnableWebSecurity //tells spring enable websecurity support throught this class
+public class SecurityConfig {
+
+    private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    public void setUserDetailsService(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(){
+
+    }
+    
+}
